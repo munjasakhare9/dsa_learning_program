b4ecea50cc468b5575f461aea4dddb7de09c4755
Yogesh Sakhare <154612218+YogeshSakhare@users.noreply.github.com>
2026-09-07 16:14:38 +0530

    count number of set bits in number

---
+++ bit_manipulation/CountSetBits.java
@@ -0,0 +1,17 @@
+class CountSetBits{
+	static int countSetBits(int n){
+		int temp=n;
+		int count=0;
+		while(temp!=0){
+			if(temp%2!=0){
+				count++;
+			}
+			temp=temp>>1;
+		}
+		return count;
+	}
+	
+	public static void main(String args[]){
+		System.out.println("Number of set bits are :- "+countSetBits(15));
+	}
+}
